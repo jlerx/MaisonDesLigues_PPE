1ba9d7ef8d8058ed5065bfb3a36fbdf9e98aa064
Leo Cormoreche <leocormo@gmail.com>
2021-04-28 19:20:15 +0200

    Fix : redirection a la page d'acceuil quand on se connecte

--- src/Security/LoginFormAuthentificatorAuthenticator.php
+++ src/Security/LoginFormAuthentificatorAuthenticator.php
@@ -92,12 +92,15 @@ class LoginFormAuthentificatorAuthenticator extends AbstractFormLoginAuthenticat
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
+        /** Ancien Code
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);*/
+
+        return new RedirectResponse($this->urlGenerator->generate('index'));
     }
 
     protected function getLoginUrl()
